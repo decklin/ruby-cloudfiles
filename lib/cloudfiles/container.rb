ee3e92e5fda35c7453f2a5c210c73768f7f3201f
Decklin Foster <decklin@red-bean.com>
2012-04-20 17:32:19 -0400

    Fix full_listing logic

--- lib/cloudfiles/container.rb
+++ lib/cloudfiles/container.rb
@@ -221,12 +221,12 @@ module CloudFiles
       params[:marker] ||= params[:offset] unless params[:offset].nil?
       query = []
       params.each do |param, value|
-        if [:limit, :marker, :prefix, :path, :delimiter].include? param
+        if [:full_listing, :limit, :marker, :prefix, :path, :delimiter].include? param
           query << "#{param}=#{CloudFiles.escape(value.to_s)}"
         end
       end
       begin
-        response = SwiftClient.get_container(self.connection.storageurl, self.connection.authtoken, escaped_name, params[:marker], params[:limit], params[:prefix], params[:delimiter])
+        response = SwiftClient.get_container(self.connection.storageurl, self.connection.authtoken, escaped_name, params[:marker], params[:limit], params[:prefix], params[:delimiter], params[:full_listing])
         return response[1].collect{|o| o['name']}
       rescue ClientException => e
         raise CloudFiles::Exception::InvalidResponse, "Invalid response code #{e.status}" unless (e.status.to_s == "200")
@@ -256,12 +256,12 @@ module CloudFiles
       params[:marker] ||= params[:offset] unless params[:offset].nil?
       query = ["format=xml"]
       params.each do |param, value|
-        if [:limit, :marker, :prefix, :path, :delimiter].include? param
+        if [:full_listing, :limit, :marker, :prefix, :path, :delimiter].include? param
           query << "#{param}=#{CloudFiles.escape(value.to_s)}"
         end
       end
       begin 
-        response = SwiftClient.get_container(self.connection.storageurl, self.connection.authtoken, escaped_name, params[:marker], params[:limit], params[:prefix], params[:delimiter])
+        response = SwiftClient.get_container(self.connection.storageurl, self.connection.authtoken, escaped_name, params[:marker], params[:limit], params[:prefix], params[:delimiter], params[:full_listing])
         return Hash[*response[1].collect{|o| [o['name'],{ :bytes => o["bytes"], :hash => o["hash"], :content_type => o["content_type"], :last_modified => DateTime.parse(o["last_modified"])}] }.flatten]
       rescue ClientException => e
         raise CloudFiles::Exception::InvalidResponse, "Invalid response code #{e.status}" unless (e.status.to_s == "200")
